b07936d42f97bc33e40223a711018fd00157939d
Raman Rai <ramankumarrai7@gmail.com>
2022-08-07 14:47:28 +0530

    Updated Readme. Created Spring Boot App

---
+++ src/test/java/raman/spring/jokesapp/JokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package raman.spring.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
